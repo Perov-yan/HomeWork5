b439cf3cb404dee492c0101b565026a753591c33
Ян <peroff007@gmail.com>
2024-03-17 01:17:11 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Здание 1:");
-        byte ClientOS = 1;
-        switch (ClientOS){
+        byte clientOS = 1;
+        switch (clientOS){
             case 0:
                 System.out.println("Установите версию приложения для iOS по ссылке");
                 break;
@@ -13,5 +13,18 @@ public class Main {
             }
         System.out.println(" ");
 
+        System.out.println("Здание 2:");
+        short clientDeviceYear = 2010;
+        var OS = "Operating System";
+        if (clientOS == 0){
+            OS = "IOS";
+        }   else {
+            OS = "Android";
+        }
+        if (clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для " + OS + " по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для " + OS + " по ссылке");
+        }
     }
 }
